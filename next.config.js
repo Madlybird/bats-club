6e812560b531b0e3a730c44cd5911409f7481838
kiwib <kiwib@users.noreply.github.com>
2026-04-09 23:50:46 +0700

    add Supabase Storage domain to next.config.js images remotePatterns

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "rnlnnunmzikpysstsywx.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+      {
+        protocol: "https",
         hostname: "**",
       },
       {
